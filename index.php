1e0302625a00860ce4d4e539b1f3e45fe0ebf651
WWology <ww@wwology.tech>
2021-07-26 05:29:24 +0000

    initial commit

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-<?php
-/*-------------------------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------------------------------------------------------------------------*/
-
-function hello($name) {
-	echo "Hello $name!";
-}
-
-hello('remote world');
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Score It</title>
+</head>
+<body>
+    <h1>Score IT</h1>
+</body>
+</html>
