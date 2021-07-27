5c0dbd72ed59060e0e21807a007f39be199d4d99
WWology <ww@wwology.tech>
2021-07-27 14:34:54 +0000

    landing-page done, moving on to the main content

--- app/index.php
+++ app/index.php
@@ -1,12 +1,44 @@
-<!-- <!DOCTYPE html>
+<?php
+session_start();
+
+function startGame() {
+    $name = $_POST['fullName'];
+    $subject = $_POST['subject'];
+
+    echo "{$name}, {$subject}";
+}
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="stylesheet" href="styles.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Score It</title>
 </head>
+
 <body>
-    <h1>Score IT</h1>
+    <div id="landing-page">
+        <h1 class="title">Score IT</h1>
+        <?php
+            if(isset($_POST['playButton']))
+            {
+                startGame();
+            }
+        ?>
+        <form class="start-form" method="post">
+            <input type="text" name="fullName" placeholder="Please enter your fullname here" /><br>
+            <label for="subject">Choose a subject: </label>
+            <select name="subject" id="subject">
+                <option value="mathematics">Mathematics</option>
+                <option value="literature">Literature</option>
+            </select>
+            <br>
+            <button type="submit" name="playButton" value="play">Play Now</button>
+        </form>
+    </div>
 </body>
-</html> -->
+
+</html>
